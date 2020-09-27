0d330cba30a221cb7a0c6749301133c0331255df
alicheaibx <41730625@students.liu.edu.lb>
2020-09-27 16:38:54 +0300

    .,,,,,,,,

--- case.php
+++ case.php
@@ -2,34 +2,28 @@
 $con = mysqli_connect( 'localhost', 'root', '', 'security_db' );
 // Check connection
 if ( mysqli_connect_errno() )
-{
+ {
     echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
 }
 
+$sql = 'SELECT * FROM `case`;';
 
-					
-			
-                    $sql = "SELECT * FROM `case`;";
+if ( $result = mysqli_query( $con, $sql ) )
+ {
+    if ( !$result ) {
+        die( 'query failed' );
 
-			
-		
-
-    if ( $result = mysqli_query( $con, $sql ) )
-    {	if (!$result) {
-		die("query failed");
-	
-        }
-        $resultCheck=mysqli_num_rows($result);
-        $emparray = array();
-        while( $row = mysqli_fetch_assoc( $result ) )
-        $emparray[] = $row;
-
-        echo( json_encode( $emparray ) );
-
-        // Free result set
-        mysqli_free_result( $result );
     }
+    $resultCheck = mysqli_num_rows( $result );
+    $emparray = array();
+    while( $row = mysqli_fetch_assoc( $result ) )
+    $emparray[] = $row;
 
+    echo( json_encode( $emparray ) );
+
+    // Free result set
+    mysqli_free_result( $result );
+}
 
 mysqli_close( $con );
 
